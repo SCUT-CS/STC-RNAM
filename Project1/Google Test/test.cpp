bbf3a65d1c4b5649d9142144ad36c63db0affaf1
liukanshan1024/liukanshan1024 <85719360+liukanshan1@users.noreply.github.com>
2022-04-17 11:25:50 +0800

    修改测试PSNR

--- Project1/Google Test/test.cpp
+++ Project1/Google Test/test.cpp
@@ -3,7 +3,6 @@
 #include "../Project1/Tree.cpp"
 //#include "../Project1/Calculate.h"
 //#include "../Project1/Calculate.cpp"
-#include "../Project1/Test.cpp"
 namespace test 
 {
 	/** Test tree create and add node
@@ -32,23 +31,34 @@ namespace test
 		EXPECT_NE(*p, nullptr);
 		EXPECT_NE(*p, *it);
 	}
-	/** Test Calaulate::PSNR
+	/** Test Calaulate::PSNR1
       * @author CuiYuxin */
-	TEST(TestCalculate, TestPSNR)  //TODO: 相对文件路径问题
+	TEST(TestCalculate, TestPSNR1)
 	{
-		//string image_path1 = samples::findFile("../Project1/gray_images/1_gray256.bmp");
-		//Mat img1 = imread(image_path1, 0);
-		//Mat imggest1 = Mat::zeros(img1.size(), img1.type());
-		//double res1 = PSNR(img1, imggest1);
-		//EXPECT_EQ(res1, 2.2584888242135288);
-		//
-		//string image_path2 = samples::findFile("../Project1/gray_images/bigbuildingzyp256.bmp");
-		//Mat img2 = imread(image_path2, 0);
-		//Mat imggest2 = Mat::zeros(img2.size(), img2.type());
-		//double res2 = PSNR(img2, imggest2);
-		//EXPECT_EQ(res1, 2.0676017009525003);
+		//include <windows.h>
+		//与 namespace cv 冲突
+		//int bufferSize = GetCurrentDirectory(0, NULL);
+		//TCHAR* dir = new TCHAR[bufferSize];
+		//GetCurrentDirectory(bufferSize, dir);
+		//CurrentDirectory:"C:\Users\L-kanshan\OneDrive - arocyx\文档\GitHub\Project-1\Project1\Project1\x64\Debug"
+		
+		string image_path1 = samples::findFile("../../gray_images/1_gray256.bmp");
+		Mat img1 = imread(image_path1, 0);
+		Mat imggest1 = Mat::zeros(img1.size(), img1.type());
+		double res1 = PSNR(img1, imggest1);
+		EXPECT_EQ(res1, 2.2584888242135288);
 	}
 
+	/** Test Calaulate::PSNR1
+      * @author CuiYuxin */
+	TEST(TestCalculate, TestPSNR2)
+	{
+		string image_path2 = samples::findFile("../../gray_images/bigbuildingzyp256.bmp");
+		Mat img2 = imread(image_path2, 0);
+		Mat imggest2 = Mat::zeros(img2.size(), img2.type());
+		double res2 = PSNR(img2, imggest2);
+		EXPECT_EQ(res2, 2.0676017009525007);
+	}
 }
 
 int main(int argc, char** argv) {
--- Project1/Project1/Calculate.h
+++ Project1/Project1/Calculate.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Headers.h"
+
 class Calculate
 {
 public:
