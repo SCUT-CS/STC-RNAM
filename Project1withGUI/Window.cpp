6d1da2fa0b8ba37a554d2b6ee0f6669b933081ef
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-05-16 11:18:08 +0000

    Update Window.cpp

--- Project1withGUI/Window.cpp
+++ Project1withGUI/Window.cpp
@@ -1,7 +1,7 @@
 ﻿#include "Window.h"
 
-
-
+/** Main window.
+  * @author CuiYuxin YangYaocheng */
 Window::Window(QWidget *parent)
     : QMainWindow(parent)
 {
@@ -11,16 +11,22 @@ Window::Window(QWidget *parent)
     connect(ui.about, SIGNAL(triggered()), this, SLOT(about()));
 }
 
+/** Open a image file.
+  * @author CuiYuxin YangYaocheng */
 void Window::openFile()
 {
     fileName = QFileDialog::getOpenFileName(this, QString("打开图片"), QDir::homePath(), QString("Images (*.jpg *.bmp *.png *.jpeg *.gif)"));
 }
 
+/** Save a image file.
+  * @author CuiYuxin YangYaocheng */
 void Window::saveFile()
 {
     // TODO
 }
 
+/** Open an about Window.
+  * @author CuiYuxin YangYaocheng */
 void Window::about()
 {
     QString content = "// TODO";
